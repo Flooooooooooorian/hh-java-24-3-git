2a68b749c8308acfdf05109b20f508e1b70b2f09
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-06-26 14:35:27 +0200

    Add sout java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("Version Kontrolle");
+        System.out.println("Java");
     }
 }
